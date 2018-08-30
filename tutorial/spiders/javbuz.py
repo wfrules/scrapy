3dd389c033d0d42e1a3715a78f97f092397703bb
wfrules <37941044@qq.com>
2018-08-30 17:38:55 +0800

    域名判断处理

--- tutorial/spiders/javbuz.py
+++ tutorial/spiders/javbuz.py
@@ -7,7 +7,7 @@ from tutorial.items import JavBuzItem
 
 class JavBuzSpider(scrapy.Spider):
     name = "javbuz"
-    allowed_domains = ["javbuz.com/"]
+    allowed_domains = ["javbuz.com"]
     start_urls = ['http://javbuz.com/movie?sort=published&page=1']
 
     def parse(self, response):
