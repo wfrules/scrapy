a9d6c93e0b483a94a8e747e9b4a723398d6e8f12
wfrules <37941044@qq.com>
2018-09-05 23:21:00 +0800

    代理池验证

---
+++ tutorial/spiders/validate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from django_orm.app.models import Proxy
+from scrapy.http import Request
+from tutorial.items import ProxyItem
+import datetime
+
+
+class ValidateSpider(scrapy.Spider):
+    name = "validate"
+    allowed_domains = ["www.baidu.com"]
+    # start_urls = ['http://www.baidu.com/']
+
+    def start_requests(self):
+        arrProxys = Proxy.objects.all()
+        for index, aProxy in enumerate(arrProxys):
+            aProxyItem = ProxyItem()
+            aProxyItem['id'] = aProxy.id
+            aProxyItem['ip'] = aProxy.ip
+            aProxyItem['port'] = aProxy.port
+            aProxyItem['request_at'] = datetime.datetime.now()
+            aProxyItem['response_at'] = None
+            aProxyItem['area'] = aProxy.area
+            aProxyItem['remark'] = aProxy.remark
+            yield aProxyItem
+            yield Request(url='https://www.baidu.com',  meta={'item': aProxyItem}, callback=self.parse)
+
+    def parse(self, response):
+        aProxyItem = response.meta['item']
+        aProxyItem['response_at'] = datetime.datetime.now()
+        yield aProxyItem
