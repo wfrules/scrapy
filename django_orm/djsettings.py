a0366a42766a000f828e86a275648d0da2dfe387
wfrules <37941044@qq.com>
2018-09-05 16:56:13 +0800

    设置动态获取

--- django_orm/djsettings.py
+++ django_orm/djsettings.py
@@ -2,15 +2,19 @@
 
 SECRET_KEY = '123'
 
+import os
+import json
+objConfigFile = open(os.getcwd() + "/cliconfig.json", encoding='utf-8')  # 设置以utf - 8 解码模式读取文件，encoding参数必须设置
+objSetting = json.load(objConfigFile)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': objSetting['db'],
+        'USER': objSetting['user'],
+        'PASSWORD': objSetting['passwd'],
+        'HOST': objSetting['host'],
+        'PORT': objSetting['port'],
         'OPTIONS': {'charset': 'utf8'}
     },
 }
